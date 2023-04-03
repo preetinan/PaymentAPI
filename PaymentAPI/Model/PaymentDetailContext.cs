e86d90b1a2e3c97983e89cb8b14638ad284b3a99
Preeti <Preeti@DESKTOP-S0CGGG1>
2023-04-03 16:45:47 -0500

    Add project files.

---
+++ PaymentAPI/Model/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace PaymentAPI.Model
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext (DbContextOptions<PaymentDetailContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
---
+++ PaymentAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PaymentAPI.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PaymentDetailContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<PaymentDetailContext>();
+builder.Services.AddCors();
+
+var app = builder.Build();
+app.UseCors(options => options.WithOrigins("http://localhost:51679").AllowAnyMethod().AllowAnyHeader());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
